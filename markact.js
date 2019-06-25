db6086d20df89528b8363c25af3dc5d4546598db
Mark Kuczmarski <markkuczmarski@gmail.com>
2019-06-25 14:52:04 +0900

    Update markact.js

--- markact.js
+++ markact.js
@@ -13,13 +13,17 @@ export function render(vnode) {
     if (vnode.split) return document.createTextNode(vnode);
     let n = document.createElement(vnode.nodeName);
     let as = vnode.attributes || {};
+    let onRender = null;
     for (let k in as) {
-        if (typeof as[k] === "function") {
+        if (k === 'onRender') {
+            onRender = as[k];
+        } else if (typeof as[k] === "function") {
             n[k] = as[k];
         } else {
             n.setAttribute(k, as[k]);
         }
     }
+    if(onRender) onRender(n);
     (vnode.children || []).map(c => n.appendChild(render(c)));
     return n;
 }
